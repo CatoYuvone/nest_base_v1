4cc6b0299f43e3552ac48a2d6f1e28f6625f7db9
Cato Yuvone <cato.yuvone@codice.dev>
2024-05-03 15:11:39 -0300

    Creado el settings.ts

---
+++ settings.ts
@@ -0,0 +1,14 @@
+import dotenv from "dotenv"
+
+dotenv.config();
+
+export const dbSettings = Object.freeze({
+    NAME: process.env.DB_NAME,
+    CONNECTOR: process.env.DB_CONNECTOR,
+    URL: process.env.DB_URL,
+    HOST: process.env.DB_HOST,
+    PORT: +process.env.DB_PORT!,
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASSWORD,
+    DATABASE: process.env.DB_DATABASE
+})
