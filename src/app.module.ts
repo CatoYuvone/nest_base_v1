823d99120edd0fc31bf39b626644ecb788b201b3
Cato Yuvone <cato.yuvone@codice.dev>
2024-05-03 15:11:49 -0300

    Agregado typeorm a app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dbSettings } from 'settings';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: dbSettings.HOST,
+      port: dbSettings.PORT,
+      username: dbSettings.USER,
+      password: dbSettings.PASSWORD,
+      entities: [],
+      database: dbSettings.DATABASE,
+      synchronize: true,
+      logging: true,
+    }),
+
+    UserModule
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
