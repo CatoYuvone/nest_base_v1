e41ca08e36009f1ee659b7adb0cb5dc9734a7314
Cato Yuvone <cato.yuvone@codice.dev>
2024-05-03 15:58:43 -0300

    Completado el create-user.dto.ts y el user.entity.ts

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,40 @@
-export class CreateUserDto {}
+import {
+    IsAlphanumeric,
+    IsEmail,
+    IsEnum,
+    IsInt,
+    IsNotEmpty,
+    IsString,
+    Matches,
+    MinLength,
+  } from 'class-validator';
+import { Roles } from '../roles.enum';
+  
+  
+  export class CreateUserDto {
+    @IsString()
+    @MinLength(2, { message: 'Name must have atleast 2 characters.' })
+    @IsNotEmpty()
+    name: string;
+  
+    @IsNotEmpty()
+    @MinLength(3, { message: 'Username must have atleast 3 characters.' })
+    @IsAlphanumeric('en-US',{
+      message: 'Username does not allow other than alpha numeric chars.',
+    })
+    username: string;
+  
+    @IsNotEmpty()
+    @IsEmail({}, { message: 'Please provide valid Email.' })
+    email: string;
+  
+    @IsInt()
+    age: number;
+  
+    @IsString()
+    @IsEnum(Roles)
+    rol: string;
+  
+    @IsNotEmpty()
+    password: string;
+  }
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,31 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Roles } from '../roles.enum';
+
+@Entity()
+export class User {
+  /**
+   * this decorator will help to auto generate id for the table.
+   */
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 30 })
+  name: string;
+
+  @Column({ type: 'varchar', length: 15 })
+  username: string;
+
+  @Column({ type: 'varchar', length: 40 })
+  email: string;
+
+  @Column({ type: 'int' })
+  age: number;
+
+
+  @Column({ type: 'varchar' })
+  password: string;
+
+  @Column({type:'enum',enum:Roles})
+  rol:string;
+
+}
